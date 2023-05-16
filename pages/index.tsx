3e6e12371ea92158ced66810659b23891174720e
Dajlan Zaimaj <51968936+dailani@users.noreply.github.com>
2023-05-16 12:05:59 +0200

    seperate landing page and app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,20 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
-import utilStyles from "../styles/utils.module.css";
 import Navbar from "../components/navbar/navbar.js";
-import { Profile } from "../components/profile";
-import { SendTransaction } from "../components/transaction/send-transaction";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
-    <Layout home>
+    <Layout>
       <Navbar></Navbar>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>Welcome to Nexus Vaults! Please connect your wallet</p>
-      </section>
-      <Profile />
-      <SendTransaction />
+      <div>
+        <button onClick={() => router.push("/launch-app")}>Launch app</button>
+      </div>
     </Layout>
   );
 }
