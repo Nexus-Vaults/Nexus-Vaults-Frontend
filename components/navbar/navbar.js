4f3c2956d53922db3a1197ecc8f017dfe25e3c91
Dajlan Zaimaj <51968936+dailani@users.noreply.github.com>
2023-05-19 22:11:14 +0200

    chore: more changes

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,4 +1,4 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 const Navbar = () => {
   return (
@@ -6,19 +6,25 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
-            <Link href="/" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+            <Link
+              href="/"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+            >
               Logo
             </Link>
           </div>
           <div className="flex items-center">
-            <Link href="/app" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium ml-auto">
+            <Link
+              href="/app"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium ml-auto"
+            >
               Launch App
             </Link>
           </div>
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
