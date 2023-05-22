9bd6d28281b5a03e5a5de84923e861bfc2395b4e
IPowerW <Leon@wenderoth.de>
2023-05-23 01:18:02 +0200

    add installFacetFromCatalog and batchInstallFacetFromCatalog

--- pages/app/deploy-nexus/index.tsx
+++ pages/app/deploy-nexus/index.tsx
@@ -5,8 +5,10 @@ import FeaturesSelection from "../../../components/app/nexusDeployment/FeaturesS
 import DeployNexus from "../../../components/app/nexusDeployment/deploy/DeployNexus";
 import Layout from "../../../components/layout";
 import { NextPageWithLayout } from "../../_app";
-import {useAccount, useContractWrite, usePrepareContractWrite} from "wagmi";
+import { useAccount, useContractWrite, usePrepareContractWrite } from "wagmi";
 import NexusFactory from "abiTypes/contracts/nexus/NexusFactory.sol/NexusFactory";
+import Nexus from "abiTypes/contracts/nexus/Nexus.sol/Nexus";
+import { apiClient } from "../../../API";
 
 type Props = {};
 
@@ -54,16 +56,58 @@ const Index: NextPageWithLayout = (props: Props) => {
     console.log(currentStep);
   }
 
-  const { address} = useAccount()
+  const { address } = useAccount();
 
-  const {config} = usePrepareContractWrite({
+  const { config: nexusConfig } = usePrepareContractWrite({
     address: process.env.CONTRACTADD,
     abi: NexusFactory,
     functionName: "create",
-    args: [nexusName, address!]
-  })
+    args: [nexusName, address!],
+  });
 
-  const {write,error} = useContractWrite(config)
+  const { write: writeNexus, error: errorNexus } =
+    useContractWrite(nexusConfig);
+
+  // @ts-ignore
+  function getfeatureAddress(features) {
+    return [`0x$0000`] as const;
+  }
+
+  // @ts-ignore
+  function getfeaturePayment(features) {
+    return [
+      {
+        token: `0x000` as const,
+        amount: BigInt(0),
+      },
+    ] as const;
+  }
+
+  const { config: featureConfigOne } = usePrepareContractWrite({
+    address: process.env.CONTRACTADD,
+    abi: Nexus,
+    functionName: "installFacetFromCatalog",
+    args: [
+      apiClient.getCatalogAddress(),
+      getfeatureAddress(features)[0],
+      getfeaturePayment(features)[0],
+    ],
+  });
+
+  const { config: featureConfigMany } = usePrepareContractWrite({
+    address: process.env.CONTRACTADD,
+    abi: Nexus,
+    functionName: "batchInstallFacetFromCatalog",
+    args: [
+      apiClient.getCatalogAddress(),
+      getfeatureAddress(features),
+      getfeaturePayment(features),
+    ],
+  });
+
+  const { write: writeFeature, error: errorFeature } = useContractWrite(
+    features.length === 1 ? featureConfigOne : featureConfigMany
+  );
 
   return (
     <div className="flex flex-col flex-wrap justify-start content-center bg-background  gap-2  ">
