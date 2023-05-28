30a02a91debb732e775f73bd1a2f4fba2ad73c84
IPowerW <Leon@wenderoth.de>
2023-05-28 18:14:57 +0200

    move wagmi hooks and adjusted some ui

--- components/app/nexusDeployment/deploy/DeployNexusCard.tsx
+++ components/app/nexusDeployment/deploy/DeployNexusCard.tsx
@@ -1,12 +1,95 @@
 import React from 'react';
+import { useAccount, useContractWrite, usePrepareContractWrite } from 'wagmi';
+import { schema as NexusFactory } from 'abiTypes/contracts/nexus/NexusFactory.sol/NexusFactory';
+import { schema as Nexus } from 'abiTypes/contracts/nexus/Nexus.sol/Nexus';
+import { apiClient } from 'api';
 
 type Props = {
   nexusName: string;
-
+  approved: boolean;
+  features: string[];
   featuresCount: number;
+  connected: boolean;
+  handleName: (name: string) => void;
 };
 
-const DeployNexusCard = ({ nexusName, featuresCount }: Props) => {
+// @ts-ignore
+const DeployNexusCard = ({
+  nexusName,
+  features,
+  featuresCount,
+  approved,
+  connected,
+  handleName,
+}: Props) => {
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    handleName(event.target.value);
+  };
+
+  const { address } = useAccount();
+
+  const { config: configNexus, error: errorNexus } = usePrepareContractWrite({
+    address: process.env.NEXT_PUBLIC_CONTRACT_ADD,
+    abi: NexusFactory,
+    functionName: 'create',
+    args: [nexusName, address!],
+  });
+
+  const { write: writeNexus } = useContractWrite(configNexus);
+
+  function deployNexus() {
+    writeNexus?.();
+    console.log('deploying...');
+    console.log(configNexus);
+    console.log(errorNexus);
+    console.log(process.env.NEXT_PUBLIC_CONTRACT_ADD);
+    console.log(nexusName);
+  }
+
+  function getFeatureAddress(features: string[]) {
+    return [`0x$0000`] as const;
+  }
+
+  function getFeaturePayment(features: string[]) {
+    return [
+      {
+        token: `0x000` as const,
+        amount: BigInt(0),
+      },
+    ] as const;
+  }
+
+  const { config: featureConfigOne } = usePrepareContractWrite({
+    address: process.env.NEXT_PUBLIC_CONTRACT_ADD,
+    abi: Nexus,
+    functionName: 'installFacetFromCatalog',
+    args: [
+      apiClient.getCatalogAddress(),
+      getFeatureAddress(features)[0],
+      getFeaturePayment(features)[0],
+    ],
+  });
+
+  const { config: featureConfigMany } = usePrepareContractWrite({
+    address: process.env.NEXT_PUBLIC_CONTRACT_ADD,
+    abi: Nexus,
+    functionName: 'batchInstallFacetFromCatalog',
+    args: [
+      apiClient.getCatalogAddress(),
+      getFeatureAddress(features),
+      getFeaturePayment(features),
+    ],
+  });
+
+  const { write: writeFeature, error: errorFeature } = useContractWrite(
+    features.length === 1 ? featureConfigOne : featureConfigMany
+  );
+
+  function deployFeature() {
+    writeFeature?.();
+    console.log('deploying feature...');
+  }
+
   return (
     <div className="flex flex-col flex-1 gap-2">
       <div className="flex-1 flex flex-col p-5">
@@ -18,11 +101,34 @@ const DeployNexusCard = ({ nexusName, featuresCount }: Props) => {
       <div className="flex-1 flex flex-col justify-center ">
         <div className="flex flex-row  border-solid border-2 border-black rounded-md py-1 px-3 ">
           <p>Name:</p>
-          <p>{nexusName}</p>
+          {
+            //todo: remove black border of input field
+          }
+          <input
+            className="w-[60%]"
+            placeholder={nexusName}
+            onChange={handleInputChange}
+          ></input>
         </div>
       </div>
       <div className="flex flex-row justify-center  ">
-        <button className="text-white bg-[#0e76fd] h-[40px] shadow-lg rounded-xl   font-bold py-1 px-3 inline-block ">
+        <button
+          className="text-white bg-[#0e76fd] h-[40px] shadow-lg rounded-xl   font-bold py-1 px-3 inline-block "
+          onClick={() => deployNexus()}
+          disabled={!connected || !approved}
+          //todo: proper styling for this, move to global
+          style={
+            !connected || !approved
+              ? {
+                  backgroundColor: 'grey',
+                  color: 'white',
+                }
+              : {
+                  backgroundColor: 'blue',
+                  color: 'white',
+                }
+          }
+        >
           Deploy Nexus
         </button>
       </div>
