bd17438b6ecb53a7467ae67ef7054fbc23c4ce1a
IPowerW <Leon@wenderoth.de>
2023-05-28 18:06:07 +0200

    change enum "Chain" to type and definition

--- API.ts
+++ API.ts
@@ -1,14 +1,22 @@
+import { polygon, moonbeam, localhost } from 'wagmi/chains';
 export abstract class ApiClient {
   abstract getCatalogAddress(): `0x${string}`;
   abstract getVaults(nexus: Nexus): [Vault];
   abstract getContracts(): ChainDeployment;
 }
 
-export enum Chain {
-  lol,
-  elias,
-  stinkt,
-}
+export type Chain = keyof typeof CHAIN_DEFINITIONS;
+
+export const CHAIN_DEFINITIONS = {
+  Localhost: { id: 1337, name: 'localhost', chain: localhost },
+  Moonbeam: { id: 1, name: 'moonbeam', chain: moonbeam },
+  Polygon: { id: 1, name: 'polygon', chain: polygon },
+  Ethereum: { id: 1, name: 'polygon', chain: polygon },
+  Ada: { id: 1, name: 'polygon', chain: polygon },
+  Binance: { id: 1, name: 'polygon', chain: polygon },
+  Bitcoin: { id: 1, name: 'polygon', chain: polygon },
+  USDT: { id: 1, name: 'polygon', chain: polygon },
+} as const;
 interface TokenBalance {
   token: string;
   total: number;
@@ -42,7 +50,7 @@ class ApiClientMock extends ApiClient {
       {
         balances: [{ token: '', total: 0, liquid: 0 }],
         address: '0x000',
-        chain: Chain.lol,
+        chain: nexus.chain,
       },
     ];
   }
