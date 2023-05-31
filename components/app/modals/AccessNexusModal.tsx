17b9ee2ad886912381471d8a60c87cedfbfb04ee
Dajlan Zaimaj <51968936+dailani@users.noreply.github.com>
2023-05-31 22:56:16 +0200

    chore: validation on access nexus

--- components/app/modals/AccessNexusModal.tsx
+++ components/app/modals/AccessNexusModal.tsx
@@ -1,11 +1,16 @@
-import React, { useRef } from 'react';
+import { useRouter } from 'next/router';
+import React, { useRef, useState } from 'react';
 
 type Props = {
   onClose: () => void;
 };
 
 const AccessNexusModal = ({ onClose }: Props) => {
-  const modalRef = useRef(null);
+  const [chainId, setChainId] = useState('');
+  const [address, setAddress] = useState('');
+  const [error, setError] = useState('');
+
+  const router = useRouter();
 
   const handleBackgroundClick = (event: any) => {
     onClose();
@@ -14,6 +19,35 @@ const AccessNexusModal = ({ onClose }: Props) => {
   const handleInnerClick = (event: any) => {
     event.stopPropagation();
   };
+
+  const handleChainIdChange = (event: any) => {
+    setChainId(event.target.value);
+  };
+
+  const handleAddressChange = (event: any) => {
+    setAddress(event.target.value);
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+
+    if (chainId.trim() === '') {
+      setError('Chain ID cannot be empty');
+      return;
+    }
+
+    if (address.trim() === '') {
+      setError('Address cannot be empty');
+      return;
+    }
+
+    router.push(`/app/${chainId}/${address}`);
+
+    setChainId('');
+    setAddress('');
+    setError('');
+  };
+
   return (
     <div
       className="fixed inset-0 flex items-center justify-center z-50 bg-gray-900 bg-opacity-50"
@@ -21,7 +55,7 @@ const AccessNexusModal = ({ onClose }: Props) => {
     >
       <div className="bg-white rounded-lg p-6" onClick={handleInnerClick}>
         <h2 className="text-2xl font-bold mb-4">Enter the Nexus Information</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
@@ -34,6 +68,7 @@ const AccessNexusModal = ({ onClose }: Props) => {
               id="chainId"
               type="text"
               placeholder="Enter Chain ID"
+              onChange={handleChainIdChange}
             />
           </div>
           <div className="mb-4">
@@ -48,6 +83,7 @@ const AccessNexusModal = ({ onClose }: Props) => {
               id="address"
               type="text"
               placeholder="Enter Address"
+              onChange={handleAddressChange}
             />
           </div>
           <div className="flex justify-end">
@@ -58,6 +94,11 @@ const AccessNexusModal = ({ onClose }: Props) => {
               Submit
             </button>
           </div>
+          {error && (
+            <p className="text-red text-center pt-10 font-mono font-bold">
+              {error}
+            </p>
+          )}
         </form>
         <button
           className="mt-4 px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600"
