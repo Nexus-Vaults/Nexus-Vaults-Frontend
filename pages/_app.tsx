60e7ff698f41ce6ed1e796ebed6c26df04c9c102
Playwo <eliaswolf2001@t-online.de>
2023-06-01 00:12:16 +0200

    Load supportedChain from backend

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,14 +7,8 @@ import {
   connectorsForWallets,
   RainbowKitProvider,
 } from '@rainbow-me/rainbowkit';
-import { configureChains, createConfig, WagmiConfig } from 'wagmi';
-import {
-  polygon,
-  fantom,
-  polygonMumbai,
-  moonbeam,
-  hardhat,
-} from 'wagmi/chains';
+import { Config, configureChains, createConfig, PublicClient, WagmiConfig, WebSocketPublicClient } from 'wagmi';
+import  * as supportedChains from 'wagmi/chains';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
 import { Chain } from '@wagmi/chains';
@@ -42,56 +36,76 @@ type AppPropsWithLayout = AppProps & {
 const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
 const alchemyId = process.env.NEXT_PUBLIC_ALCHEMY_ID;
 
-const defaultCains: Chain[] = process.env.NEXT_PUBLIC_TESTNET
-  ? [polygonMumbai, hardhat]
-  : [polygon, fantom, moonbeam];
-const { chains, publicClient } = configureChains(defaultCains, [
-  // @ts-ignore
-  alchemyProvider({ apiKey: alchemyId }),
-  publicProvider(),
-]);
+function mapEVMChainIdToChain(evmChainId: number) {
+    for (const chain of Object.values(supportedChains)) {
+      if ('id' in chain) {
+        if (chain.id === evmChainId) {
+          return chain;
+        }
+      }
+    }
 
-const connectors = connectorsForWallets([
-  {
-    groupName: 'Recommended',
-    wallets: [
-      injectedWallet({ chains }),
-      rainbowWallet({ projectId, chains }),
-      walletConnectWallet({ projectId, chains }),
-      metaMaskWallet({ projectId, chains }),
-      ledgerWallet({ projectId, chains }),
-      phantomWallet({ chains }),
-      safeWallet({ chains }),
-    ],
-  },
-]);
+    throw "Unsupported Chain";
+}
 
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors,
-  publicClient,
-});
+
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
 
   const [ deployment, setDeployment ] = useState<ChainDeployment[]>([]);
+  const [ wagmiConfig, setWagmiConfig ] = useState<Config<PublicClient, WebSocketPublicClient>>();
 
   useEffect(() => {
     apiClient.getContractsAddresses()
     .then(deployment => {
       setDeployment(deployment);
-      console.log(deployment);
+    
+      const defaultCains: Chain[] = deployment.map(x => mapEVMChainIdToChain(x.evmChainId));
+
+      const { chains, publicClient } = configureChains(defaultCains, [
+        // @ts-ignore
+        // alchemyProvider({ apiKey: alchemyId }),
+        publicProvider(),
+      ]);
+
+      const connectors = connectorsForWallets([
+        {
+          groupName: 'Recommended',
+          wallets: [
+            injectedWallet({ chains }),
+            rainbowWallet({ projectId, chains }),
+            walletConnectWallet({ projectId, chains }),
+            metaMaskWallet({ projectId, chains }),
+            ledgerWallet({ projectId, chains }),
+            phantomWallet({ chains }),
+            safeWallet({ chains }),
+          ],
+        },
+      ]);
+
+      const config = createConfig({
+        autoConnect: true,
+        connectors,
+        publicClient,
+      });
+
+      setWagmiConfig(config as any);
     } );
   }, [])
 
+
   return (
+    <>
+    {(wagmiConfig == undefined) ? 'Loading...' : (
     <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider chains={chains}>
+      <RainbowKitProvider chains={deployment.map(x => mapEVMChainIdToChain(x.evmChainId))}>
         <ChainDeployments.Provider value={deployment}>
-          {(deployment.length > 0) ? getLayout(<Component {...pageProps} />) : 'Loading...'}
+          getLayout(<Component {...pageProps} />)
         </ChainDeployments.Provider>
       </RainbowKitProvider>
     </WagmiConfig>
+    )}
+    </>
   );
 }
