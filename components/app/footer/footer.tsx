c012c1623e6f32b1afd227f238b2d7dba3e73c7e
StivenBaci <stivenbaci@gmail.com>
2023-05-31 22:50:00 +0200

    Add: footer.tsx

--- components/app/footer/footer.tsx
+++ components/app/footer/footer.tsx
@@ -1,8 +1,21 @@
 // import MenuIcon from '@mui/icons-material/Menu';
 // import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import Link from 'next/link';
 
 const Footer = () => {
-  return <div className="grid w-full bg-purple h-6"></div>;
+  return (
+    <>
+      <div className="grid grid-cols-12 w-full bg-purple h-6">
+        <Link
+          className="cols-span-1 col-end-13"
+          href="https://github.com/Nexus-Vaults/Nexus-Vaults-Frontend"
+        >
+          <GitHubIcon className="float-right" />
+        </Link>
+      </div>
+    </>
+  );
 };
 
 export default Footer;
