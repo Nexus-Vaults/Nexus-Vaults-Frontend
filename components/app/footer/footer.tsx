3ee0be36e8af97fe7dca7b107b34356dc2b9dade
StivenBaci <stivenbaci@gmail.com>
2023-05-31 15:20:27 +0200

    Add empty footer section

---
+++ components/app/footer/footer.tsx
@@ -0,0 +1,11 @@
+// import MenuIcon from '@mui/icons-material/Menu';
+// import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+
+
+const Footer = () => {
+  return (
+    <></>
+  );
+};
+
+export default Footer;
