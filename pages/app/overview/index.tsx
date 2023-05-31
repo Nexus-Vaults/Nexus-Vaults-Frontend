13b7f8320ae53065deb8e1884557e63dc8d1c766
IPowerW <Leon@wenderoth.de>
2023-05-31 22:38:17 +0200

    add Search for your Nexus

--- pages/app/overview/index.tsx
+++ pages/app/overview/index.tsx
@@ -1,8 +1,12 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useContext, useEffect, useState } from 'react';
 import Layout from '../../../components/layout';
 import { NextPageWithLayout } from '../../_app';
 import Table from '../../../components/app/table';
 import { useAccount } from 'wagmi';
+import { ChainDeployment } from 'api';
+import { ChainDeployments } from '../ContractsAddressesContext';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 type Props = {};
 
@@ -39,25 +43,20 @@ const Index: NextPageWithLayout = (props: Props) => {
   //   write: writeSetPrimaryVaultGatewayV1,
   //   error: errorSetPrimaryVaultGatewayV1,
   // } = useContractWrite(vaultV1FacetConfig);
-  const { address } = useAccount();
 
-  const tableData = [
-    { assetName: 'Asset 1', amount: 100, value: 100 },
-    { assetName: 'Asset 2', amount: 200, value: 100 },
-    { assetName: 'Asset 3', amount: 300, value: 100 },
-    { assetName: 'Asset 3', amount: 300, value: 100 },
-    { assetName: 'Asset 3', amount: 300, value: 100 },
-    { assetName: 'Asset 3', amount: 300, value: 100 },
-    { assetName: 'Asset 3', amount: 300, value: 100 },
-    { assetName: 'Asset 3', amount: 300, value: 100 },
-    { assetName: 'Asset 3', amount: 300, value: 100 },
-    { assetName: 'Asset 3', amount: 300, value: 100 },
-    { assetName: 'Asset 3', amount: 300, value: 100 },
-    { assetName: 'Asset 3', amount: 300, value: 100 },
-    { assetName: 'Asset 3', amount: 300, value: 100 },
-    { assetName: 'Asset 3', amount: 300, value: 100 },
-  ];
+  const contractsAddresses = useContext(ChainDeployments);
 
+  const [selectedItem, setSelectedItem] = useState<ChainDeployment>(
+    contractsAddresses[0]
+  );
+
+  const [nexusAddress, setInput] = useState<string>('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(event.target.value);
+  };
+
+  const router = useRouter();
   return (
     <div className="flex flex-col flex-wrap justify-center content-center bg-whitesmoke  gap-2 h-screen">
       <div className="flex-flex-col w-fit  bg-white shadow-2xl border-solid border-2 border-gray-400 rounded-lg p-2">
@@ -70,18 +69,56 @@ const Index: NextPageWithLayout = (props: Props) => {
               <div className="flex flex-row justify-center">
                 <div className="w-[100%] h-[1px] bg-gray-300"></div>
               </div>
+              {contractsAddresses.map((x) => {
+                return (
+                  <div className="flex flex-wrap justify-center gap-6 p-2">
+                    <div
+                      className={`border p-2 rounded ${
+                        selectedItem?.contractChainId === x.contractChainId
+                          ? 'bg-black'
+                          : 'hover:bg-black'
+                      }`}
+                      onClick={() => setSelectedItem(x)}
+                    >
+                      <Image
+                        src={`/public/images/${x.chainName}.png`}
+                        width={32}
+                        height={32}
+                        alt=""
+                      />
+                    </div>
+                  </div>
+                );
+              })}
               <div className="flex flex-col flex-wrap justify-center content-center  ">
                 <input
                   className="text-mono font-semibold text-center w-[60%] py-1 px-2 border-b  border-solid  hover:border-[#0e76fd] focus:border-[#0e76fd]  focus:rounded-lg  "
                   placeholder="Nexus Adress"
-                ></input>
-              </div>
-              <div className="w-full flex-1 ">
-                <Table data={tableData} />
+                  onChange={handleInputChange}
+                />
               </div>
             </div>
           </div>
         </div>
+      </div>
+      <div className="flex flex-row gap-2 justify-center">
+        <button
+          className="text-white bg-[#0e76fd] h-[40px] shadow-lg rounded-xl   font-bold py-1 px-3 inline-block hover:scale-105 transition-all duration-300"
+          onClick={() => router.push('/app')}
+        >
+          Back
+        </button>
+        <button
+          className="text-white bg-[#0e76fd] h-[40px] shadow-lg rounded-xl   font-bold py-1 px-3 inline-block hover:scale-105 transition-all duration-300"
+          onClick={() =>
+            nexusAddress.length !== 0 &&
+            router.push(
+              `/app/overview/${selectedItem.contractChainId.toString()}/${nexusAddress}`
+            )
+          }
+        >
+          Next
+        </button>
       </div>
     </div>
   );
