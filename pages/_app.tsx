8adfeaa1ce93cfd3503ff6b5b136133b63d084bf
StivenBaci <stivenbaci@gmail.com>
2023-06-01 00:40:03 +0200

    Update _app.tsx
    +formatting
    +compact wallet modalSize

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,8 +7,15 @@ import {
   connectorsForWallets,
   RainbowKitProvider,
 } from '@rainbow-me/rainbowkit';
-import { Config, configureChains, createConfig, PublicClient, WagmiConfig, WebSocketPublicClient } from 'wagmi';
-import  * as supportedChains from 'wagmi/chains';
+import {
+  Config,
+  configureChains,
+  createConfig,
+  PublicClient,
+  WagmiConfig,
+  WebSocketPublicClient,
+} from 'wagmi';
+import * as supportedChains from 'wagmi/chains';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
 import { Chain } from '@wagmi/chains';
@@ -37,31 +44,31 @@ const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
 const alchemyId = process.env.NEXT_PUBLIC_ALCHEMY_ID;
 
 function mapEVMChainIdToChain(evmChainId: number) {
-    for (const chain of Object.values(supportedChains)) {
-      if ('id' in chain) {
-        if (chain.id === evmChainId) {
-          return chain;
-        }
+  for (const chain of Object.values(supportedChains)) {
+    if ('id' in chain) {
+      if (chain.id === evmChainId) {
+        return chain;
       }
     }
+  }
 
-    throw "Unsupported Chain";
+  throw 'Unsupported Chain';
 }
-
-
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
 
-  const [ deployment, setDeployment ] = useState<ChainDeployment[]>([]);
-  const [ wagmiConfig, setWagmiConfig ] = useState<Config<PublicClient, WebSocketPublicClient>>();
+  const [deployment, setDeployment] = useState<ChainDeployment[]>([]);
+  const [wagmiConfig, setWagmiConfig] =
+    useState<Config<PublicClient, WebSocketPublicClient>>();
 
   useEffect(() => {
-    apiClient.getContractsAddresses()
-    .then(deployment => {
+    apiClient.getContractsAddresses().then((deployment) => {
       setDeployment(deployment);
-    
-      const defaultCains: Chain[] = deployment.map(x => mapEVMChainIdToChain(x.evmChainId));
+
+      const defaultCains: Chain[] = deployment.map((x) =>
+        mapEVMChainIdToChain(x.evmChainId)
+      );
 
       const { chains, publicClient } = configureChains(defaultCains, [
         // @ts-ignore
@@ -91,21 +98,25 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
       });
 
       setWagmiConfig(config as any);
-    } );
-  }, [])
-
+    });
+  }, []);
 
   return (
     <>
-    {(wagmiConfig == undefined) ? 'Loading...' : (
-    <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider chains={deployment.map(x => mapEVMChainIdToChain(x.evmChainId))}>
-        <ChainDeployments.Provider value={deployment}>
-           {getLayout(<Component {...pageProps} />)}
-        </ChainDeployments.Provider>
-      </RainbowKitProvider>
-    </WagmiConfig>
-    )}
+      {wagmiConfig == undefined ? (
+        'Loading...'
+      ) : (
+        <WagmiConfig config={wagmiConfig}>
+          <RainbowKitProvider
+            modalSize="compact"
+            chains={deployment.map((x) => mapEVMChainIdToChain(x.evmChainId))}
+          >
+            <ChainDeployments.Provider value={deployment}>
+              {getLayout(<Component {...pageProps} />)}
+            </ChainDeployments.Provider>
+          </RainbowKitProvider>
+        </WagmiConfig>
+      )}
     </>
   );
 }
