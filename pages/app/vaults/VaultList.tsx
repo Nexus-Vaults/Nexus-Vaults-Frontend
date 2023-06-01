5609d118c658d57e5726caefcc37695215c37878
Dajlan Zaimaj <51968936+dailani@users.noreply.github.com>
2023-06-01 07:57:24 +0200

    revert back

--- pages/app/vaults/VaultList.tsx
+++ pages/app/vaults/VaultList.tsx
@@ -9,28 +9,34 @@ import logo1 from '../../../public/images/chain/1284.png';
 type Props = {};
 
 const VaultList: NextPageWithLayout = (props: Props) => {
-  const rows = [
-    {
-      logo: Eth,
-      version: 'V1',
-      vaultId: 1,
-      totalAsset: 100,
-    },
-    {
-      logo: fantom,
-      version: 'V1',
-      vaultId: 2,
-      totalAsset: 839,
-    },
-    {
-      logo: logo1,
-      version: 'V1',
-      vaultId: 3,
-      totalAsset: 690,
-    },
-  ];
+  /**
+   *
+   * Coming soon feature
+   */
+  // const rows = [
 
-  return rows.map((row, index) => <VaultRows key={index} {...row} />);
+  //   {
+  //     logo: Eth,
+  //     version: 'V1',
+  //     vaultId: 1,
+  //     totalAsset: 100,
+  //   },
+  //   {
+  //     logo: fantom,
+  //     version: 'V1',
+  //     vaultId: 2,
+  //     totalAsset: 839,
+  //   },
+  //   {
+  //     logo: logo1,
+  //     version: 'V1',
+  //     vaultId: 3,
+  //     totalAsset: 690,
+  //   },
+  // ];
+
+  return <></>;
+  // return rows.map((row, index) => <VaultRows key={index} {...row} />);
 };
 
 VaultList.getLayout = function getLayout(page: ReactElement) {
