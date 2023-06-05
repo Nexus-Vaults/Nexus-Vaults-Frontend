a6e7045bf861dfa74e56f9ee3e52f8933b0225a3
Playwo <eliaswolf2001@t-online.de>
2023-06-05 14:11:32 +0200

    Cleanup env

--- enviroment.d.ts
+++ enviroment.d.ts
@@ -3,10 +3,5 @@ declare namespace NodeJS {
     readonly NEXT_PUBLIC_TESTNET: boolean;
     readonly NEXT_PUBLIC_PROJECT_ID: string;
     readonly NEXT_PUBLIC_ALCHEMY_ID: string;
-    readonly NEXT_PUBLIC_APP_NAME: string;
-    readonly NEXT_PUBLIC_NEXUS_FACTORY_ADD: `0x${string}`;
-    readonly NEXT_PUBLIC_NEXUS_GATEWAY_ADD: `0x${string}`;
-    readonly NEXT_PUBLIC_VAULT_CONTROLLER_ADD: `0x${string}`;
-    readonly NEXT_PUBLIC_CATALOG_ADD: `0x${string}`;
   }
 }
