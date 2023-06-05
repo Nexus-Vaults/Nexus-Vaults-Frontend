23ad1001d578991bcc0f21ebfc2f0cf39540d26d
Playwo <eliaswolf2001@t-online.de>
2023-06-05 13:49:30 +0200

    Fix missing docs link

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,7 @@ export default function Home() {
           </button>
           <button
             className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-white rounded-lg group bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500"
-            onClick={() => router.push('/app')}
+            onClick={() => window.open('https://farsight.gitbook.io/nexus-vaults-docs/', "_blank")}
           >
             <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
               Learn more
