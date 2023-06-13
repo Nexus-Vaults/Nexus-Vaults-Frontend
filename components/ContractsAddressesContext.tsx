71a6ded7fb62715c74292cbe057214ae7bf4cc13
IPowerW <Leon@wenderoth.de>
2023-06-13 14:05:02 +0200

    mod naming

--- components/ContractsAddressesContext.tsx
+++ components/ContractsAddressesContext.tsx
@@ -3,14 +3,14 @@ import { ChainDeployment } from 'api';
 
 export const ChainDeployments = createContext<{
   chainDeployment: ChainDeployment[];
-  chainId?: number | null;
-  address?: `0x${string}` | null;
+  contractChainId?: number | null;
+  nexusAddress?: `0x${string}` | null;
   updatedChainId: (nr: number) => void;
   updatedAddress: (add: `0x${string}`) => void;
 }>({
   chainDeployment: [],
-  chainId: null,
-  address: null,
+  contractChainId: null,
+  nexusAddress: null,
   updatedAddress: () => {},
   updatedChainId: () => {},
 });
