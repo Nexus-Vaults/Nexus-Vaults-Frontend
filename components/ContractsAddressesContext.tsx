cd2306d1f8ed992ec65d095844431c8cb2421887
IPowerW <Leon@wenderoth.de>
2023-06-13 14:13:52 +0200

    rem context content

--- components/ContractsAddressesContext.tsx
+++ components/ContractsAddressesContext.tsx
@@ -3,14 +3,6 @@ import { ChainDeployment } from 'api';
 
 export const ChainDeployments = createContext<{
   chainDeployment: ChainDeployment[];
-  contractChainId?: number | null;
-  nexusAddress?: `0x${string}` | null;
-  updatedChainId: (nr: number) => void;
-  updatedAddress: (add: `0x${string}`) => void;
 }>({
   chainDeployment: [],
-  contractChainId: null,
-  nexusAddress: null,
-  updatedAddress: () => {},
-  updatedChainId: () => {},
 });
