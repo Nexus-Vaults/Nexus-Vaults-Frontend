0ed56e192eec6c5afae631ab563fb25af7d28cee
IPowerW <Leon@wenderoth.de>
2023-06-14 23:19:30 +0200

    remove IOU

--- components/Context.tsx
+++ components/Context.tsx
@@ -1,15 +1,8 @@
 import { createContext } from 'react';
 import { ChainDeployment } from 'api';
-import { Address } from 'wagmi';
 
 export const ChainDeployments = createContext<{
   chainDeployment: ChainDeployment[];
 }>({
   chainDeployment: [],
-});
-
-export const IOUToken = createContext<{
-  IOUAddress: Address | null;
-}>({
-  IOUAddress: null,
 });
